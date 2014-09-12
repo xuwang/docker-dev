7030d66f97e15bbbe05c18540b2632b7fb541bf6
Xu Wang <xuwang@gmail.com>
2014-09-12 15:28:19 -0700

    add cleanup on exit in images/nodeapp-etcd/appreg.sh

--- images/nodeapp-etcd/appreg.sh
+++ images/nodeapp-etcd/appreg.sh
@@ -14,12 +14,18 @@ if [ "$SERVICE_PORT" == "" ];then
 fi
 
 hn=`hostname`
+etcd_addr="${ETCD_PORT_4001_TCP_ADDR}:${ETCD_PORT_4001_TCP_PORT}"
+value="{ \"host\": \"$SERVICE_IP\", \"port\": $SERVICE_PORT, \"name\": \"$hn\" }"
 
+# rm from etcd when the exit 
+function finish {
+  echo "etcdctl rm /services/nodeapp/$hn"
+  etcdctl --peers $etcd_addr rm /services/nodeapp/$hn
+}
+trap finish EXIT
+
+# the main reg loop, may be we should test nodeapp is trully up by curl it first
 while true; do
-	etcdctl \
-		--peers "${ETCD_PORT_4001_TCP_ADDR}:${ETCD_PORT_4001_TCP_PORT}" \
-		set /services/nodeapp/$hn "{ \"host\": \"$SERVICE_IP\", \"port\": $SERVICE_PORT, \"name\": \"$hn\" }" \
-		--ttl 20
-
+	etcdctl --peers $etcd_addr set /services/nodeapp/$hn "$value" --ttl 20
 	sleep 15
 done
